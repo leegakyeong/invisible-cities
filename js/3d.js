e87751a2e75d8e38a709d30a5b48bab900741970
leegakyeong <alaska7132@naver.com>
2021-04-11 18:52:27 +0900

    Animate model

--- js/3d.js
+++ js/3d.js
@@ -1,6 +1,9 @@
 import * as THREE from '../lib/three.module.js';
 import { GLTFLoader } from '../lib/GLTFLoader.js';
 
+const clock = new THREE.Clock();
+let mixer;
+
 const scene = new THREE.Scene();
 const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
 
@@ -14,6 +17,10 @@ const loader = new GLTFLoader();
 
 loader.load('models/hands.gltf', function(gltf) {
   scene.add(gltf.scene);
+  mixer = new THREE.AnimationMixer(gltf.scene);
+  gltf.animations.forEach(function(clip) {
+    mixer.clipAction(clip).play();
+  });
 }, undefined, function(err) {
   console.error(err);
 });
@@ -22,6 +29,7 @@ camera.position.z = 50;
 
 function animate() {
 	requestAnimationFrame(animate);
+  if (mixer) mixer.update(clock.getDelta());
 	renderer.render(scene, camera);
 }
 animate();
