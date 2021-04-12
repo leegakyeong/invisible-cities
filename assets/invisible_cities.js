545d4f46968c07edbc058f8b4424642e4c2d4f7a
leegakyeong <alaska7132@naver.com>
2021-04-12 17:49:59 +0900

    Format javascript

--- assets/invisible_cities.js
+++ assets/invisible_cities.js
@@ -1,62 +1,52 @@
-// var habitants_clicker = document.getElementsByClassName("habitants_clicker")[0]; 
-// var hexit_button = document.getElementsByClassName("hexit")[0];
+// const habitantsClicker = document.getElementsByClassName('habitants-clicker')[0];
+// const hexitButton = document.getElementsByClassName('hexit')[0];
 
-// habitants_clicker.addEventListener("click" , function() {
-//     console.log(1)
-//     var hab = document.getElementsByClassName("hwrapper")[0];
-//     hab.style.display = "flex";
-//     hab.style.visibility  = "visible";
-//     hab.style.zIndex = "2";
-//     hab.style.position = "relative";
-//     habitants_clicker.style.zIndex = "0";
-//     document.getElementsByClassName("container")[0].style.backgroundImage="blur(5px)";
+// habitantsClicker.addEventListener('click', function() {
+//   console.log(1);
+//   const hab = document.getElementsByClassName('hwrapper')[0];
+//   hab.style.display = 'flex';
+//   hab.style.visibility = 'visible';
+//   hab.style.zIndex = '2';
+//   hab.style.position = 'relative';
+//   habitantsClicker.style.zIndex = '0';
+//   document.getElementsByClassName('container')[0].style.backgroundImage = 'blur(5px)';
+// });
 
+// hexitButton.addEventListener('click', function() {
+//   const hab = document.getElementsByClassName('hwrapper')[0];
+//   hab.style.display = 'none';
+//   hab.style.visibility = 'hidden';
+//   habitantsClicker.style.zIndex = '1';
+// });
 
+// const dataSiloClicker = document.getElementsByClassName('data-silo-clicker')[0];
+// const dexit_button = document.getElementsByClassName('dexit')[0];
 
-// })
+// dataSiloClicker.addEventListener('click', function() {
+//   const dat = document.getElementsByClassName('dwrapper')[0];
+//   dat.style.display = 'flex';
+//   dat.style.visibility = 'visible';
+//   dat.style.zIndex = '2';
+//   dat.style.position = 'relative';
+//   dataSiloClicker.style.zIndex = '0';
+//   document.getElementsByClassName('container')[0].style.backgroundImage = 'blur(5px)';
+// });
 
-// hexit_button.addEventListener("click", function(){
+// dexit_button.addEventListener('click', function() {
+//   const dat = document.getElementsByClassName('dwrapper')[0];
+//   dat.display = 'none';
+//   dat.style.visibility = 'hidden';
+//   dat.style.zIndex = '-1';
+// });
 
-//     var hab = document.getElementsByClassName("hwrapper")[0];
-//     hab.style.display ="none";
-//     hab.style.visibility = "hidden";
-//     habitants_clicker.style.zIndex = "1";
-// })
+const slide = document.querySelector('#slide');
+slide.addEventListener('click', function() {
+  const slidedown = slide.nextElementSibling;
 
-// var dataSilo_clicker = document.getElementsByClassName("dataSilo_clicker")[0];
-// var dexit_button = document.getElementsByClassName("dexit")[0];
-
-// dataSilo_clicker.addEventListener("click" , function() {
-
-//     var dat = document.getElementsByClassName("dwrapper")[0];
-//     dat.style.display = "flex";
-//     dat.style.visibility  = "visible";
-//     dat.style.zIndex = "2";
-//     dat.style.position = "relative";
-//     dataSilo_clicker.style.zIndex = "0";
-//     document.getElementsByClassName("container")[0].style.backgroundImage="blur(5px)";
-
-
-// })
-
-// dexit_button.addEventListener("click", function(){
-//     var dat = document.getElementsByClassName("dwrapper")[0];
-//     dat.display = "none";
-//     dat.style.visibility = "hidden";
-//     dat.style.zIndex = "-1";
-// })
-
-
-var slide = document.querySelector("#slide");
-slide.addEventListener("click",
-function (){
-    slidedown = slide.nextElementSibling;
-
-    if(slidedown.style.display == "none"){
-    slidedown.style.display = "inline-block";
-    slidedown.style.heigt ="20vh";}
-    else {
-    slidedown.style.display = "none";
-    }
-}
-)
+  if (slidedown.style.display == 'none') {
+    slidedown.style.display = 'inline-block';
+    slidedown.style.heigt = '20vh';
+  } else {
+    slidedown.style.display = 'none';
+  }
+});
