e40f810af02d8a0ab5818adb1aa069fa87100b73
solboii <31411284+pangpangmandu@users.noreply.github.com>
2021-04-14 21:11:57 +0900

    data-silo update

---
+++ assets/data-silo.js
@@ -0,0 +1,22 @@
+window.addEventListener("load", ()=>{
+
+    const dataSilo = document.getElementsByClassName("data-silo");
+    const slidedown = document.getElementsByClassName("slidedown");
+
+    for (let i=0; i<dataSilo.length; i++){
+        dataSilo[i].addEventListener("click",
+        function(){
+            if(slidedown[i].classList.contains("hidden")){
+                for(let k =0; k<dataSilo.length; k++){
+                    slidedown[k].classList.add("hidden");
+                }
+                slidedown[i].classList.remove("hidden");
+            
+            }else{
+                slidedown[i].classList.add("hidden");
+            }
+        }
+        )
+    }
+
+});
