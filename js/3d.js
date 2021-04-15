eca791e11fc2cc12f960de569ba390e88c84d207
leegakyeong <alaska7132@naver.com>
2021-04-16 04:37:50 +0900

    Load every model manually

--- js/3d.js
+++ js/3d.js
@@ -2,51 +2,7 @@ import * as THREE from '../lib/three.module.js';
 import { GLTFLoader } from '../lib/GLTFLoader.js';
 import { OrbitControls } from '../lib/OrbitControls.js';
 
-const menu = {
-  'title': {
-    path: 'models/invisiblecities.gltf',
-    pos: {},
-    mixer: null,
-  },
-  'INVISIBLE CITIES': {
-    path: 'models/head.gltf',
-    pos: {},
-    mixer: null,
-  },
-  'HABITANTS': {
-    path: 'models/walkingman.gltf',
-    pos: {},
-    mixer: null,
-  },
-  'ABOUT PROJECT': {
-    path: 'models/knight.gltf',
-    pos: {},
-    mixer: null,
-  },
-  'DATA SILO': {
-    path: 'models/instagram.gltf',
-    pos: {},
-    mixer: null,
-  },
-  'INSTAGRAM': {
-    path: 'models/milk.gltf',
-    pos: {},
-    mixer: null,
-  },
-  'CREDITS': {
-    path: 'models/spoon.gltf',
-    pos: {},
-    mixer: null,
-  },
-  'FACEBOOK': {
-    path: 'models/hands.gltf',
-    pos: {},
-    mixer: null,
-  },
-};
-
 const clock = new THREE.Clock();
-// let mixer;
 
 const scene = new THREE.Scene();
 const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
@@ -62,19 +18,117 @@ scene.add(light);
 
 const loader = new GLTFLoader();
 
-for (const [key, value] of Object.entries(menu)) {
-  loader.load(value.path, function(gltf) {
-    scene.add(gltf.scene);
-    if (gltf.animations.length > 0) {
-      value.mixer = new THREE.AnimationMixer(gltf.scene);
-      gltf.animations.forEach(function(clip) {
-        value.mixer.clipAction(clip).play();
-      });
-    }
-  }, undefined, function(err) {
-    console.error(err);
-  });
-}
+// title
+let invisiblecitiesMixer;
+loader.load('models/invisiblecities.gltf', function(gltf) {
+  scene.add(gltf.scene);
+  if (gltf.animations.length > 0) {
+    invisiblecitiesMixer = new THREE.AnimationMixer(gltf.scene);
+    gltf.animations.forEach(function(clip) {
+      invisiblecitiesMixer.clipAction(clip).play();
+    });
+  }
+}, undefined, function(err) {
+  console.error(err);
+});
+
+// INVISIBLE CITIES
+let headMixer;
+loader.load('models/head.gltf', function(gltf) {
+  scene.add(gltf.scene);
+  if (gltf.animations.length > 0) {
+    headMixer = new THREE.AnimationMixer(gltf.scene);
+    gltf.animations.forEach(function(clip) {
+      headMixer.clipAction(clip).play();
+    });
+  }
+}, undefined, function(err) {
+  console.error(err);
+});
+
+// HABITANTS
+let walkingmanMixer;
+loader.load('models/walkingman.gltf', function(gltf) {
+  scene.add(gltf.scene);
+  if (gltf.animations.length > 0) {
+    walkingmanMixer = new THREE.AnimationMixer(gltf.scene);
+    gltf.animations.forEach(function(clip) {
+      walkingmanMixer.clipAction(clip).play();
+    });
+  }
+}, undefined, function(err) {
+  console.error(err);
+});
+
+// ABOUT PROJECT
+let knightMixer;
+loader.load('models/knight.gltf', function(gltf) {
+  scene.add(gltf.scene);
+  if (gltf.animations.length > 0) {
+    knightMixer = new THREE.AnimationMixer(gltf.scene);
+    gltf.animations.forEach(function(clip) {
+      knightMixer.clipAction(clip).play();
+    });
+  }
+}, undefined, function(err) {
+  console.error(err);
+});
+
+// DATA SILO
+let instagramMixer;
+loader.load('models/instagram.gltf', function(gltf) {
+  scene.add(gltf.scene);
+  if (gltf.animations.length > 0) {
+    instagramMixer = new THREE.AnimationMixer(gltf.scene);
+    gltf.animations.forEach(function(clip) {
+      instagramMixer.clipAction(clip).play();
+    });
+  }
+}, undefined, function(err) {
+  console.error(err);
+});
+
+// INSTAGRAM
+let milkMixer;
+loader.load('models/milk.gltf', function(gltf) {
+  scene.add(gltf.scene);
+  if (gltf.animations.length > 0) {
+    milkMixer = new THREE.AnimationMixer(gltf.scene);
+    gltf.animations.forEach(function(clip) {
+      milkMixer.clipAction(clip).play();
+    });
+  }
+}, undefined, function(err) {
+  console.error(err);
+});
+
+// CREDITS
+let spoonMixer;
+loader.load('models/spoon.gltf', function(gltf) {
+  scene.add(gltf.scene);
+  if (gltf.animations.length > 0) {
+    spoonMixer = new THREE.AnimationMixer(gltf.scene);
+    gltf.animations.forEach(function(clip) {
+      spoonMixer.clipAction(clip).play();
+    });
+  }
+}, undefined, function(err) {
+  console.error(err);
+});
+
+// FACEBOOK
+let handsMixer;
+loader.load('models/hands.gltf', function(gltf) {
+  scene.add(gltf.scene);
+  if (gltf.animations.length > 0) {
+    handsMixer = new THREE.AnimationMixer(gltf.scene);
+    gltf.animations.forEach(function(clip) {
+      handsMixer.clipAction(clip).play();
+    });
+  }
+}, undefined, function(err) {
+  console.error(err);
+});
 
 camera.position.z = 50;
 
@@ -83,9 +137,14 @@ controls.noZoom = true;
 
 function animate() {
 	requestAnimationFrame(animate);
-  for (const [_, value] of Object.entries(menu)) {
-    if (value.mixer) value.mixer.update(clock.getDelta());
-  }
+  if (invisiblecitiesMixer) invisiblecitiesMixer.update(clock.getDelta());
+  if (headMixer) headMixer.update(clock.getDelta());
+  if (walkingmanMixer) walkingmanMixer.update(clock.getDelta());
+  if (knightMixer) knightMixer.update(clock.getDelta());
+  if (instagramMixer) instagramMixer.update(clock.getDelta());
+  if (milkMixer) milkMixer.update(clock.getDelta());
+  if (spoonMixer) spoonMixer.update(clock.getDelta());
+  if (handsMixer) handsMixer.update(clock.getDelta());
   controls.update();
 	renderer.render(scene, camera);
 }
