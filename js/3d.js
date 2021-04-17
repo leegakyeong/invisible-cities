9b758be4563f35e44c423c493da0fd350bd06f5e
leegakyeong <alaska7132@naver.com>
2021-04-17 21:16:06 +0900

    Set gradient background

--- js/3d.js
+++ js/3d.js
@@ -8,11 +8,9 @@ const clock = new THREE.Clock();
 const scene = new THREE.Scene();
 const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
 
-const renderer = new THREE.WebGLRenderer();
+const renderer = new THREE.WebGLRenderer({ alpha: true });
 renderer.setSize(window.innerWidth, window.innerHeight);
 document.body.appendChild(renderer.domElement);
-
-scene.background = new THREE.Color('rgb(127, 84, 255)');
 
 const light = new THREE.DirectionalLight();
 scene.add(light);
