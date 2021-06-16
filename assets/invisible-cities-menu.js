344c8fa74d7faae8b2cfa804b3505734f5f7f211
leegakyeong <alaska7132@naver.com>
2021-06-16 13:46:05 +0900

    Remove console.log()

--- assets/invisible-cities-menu.js
+++ assets/invisible-cities-menu.js
@@ -9,18 +9,14 @@ for(var i=0; i<eachPost.length; i++){
 const author = document.getElementsByClassName("invisible-cities-gradient")
 
 for(var i=0; i<author.length ; i++){
-    console.log(i)
     author[i].addEventListener("click", event=> {
         var id = event.target.id
-        console.log(id)
         var index = parseInt(id.substr(3,1)) -1
-        console.log(index)
         opencontent(index)
     })
 }
 
 function opencontent(index){
-    console.log(index)
     eachPost[index].classList.remove("hidden")
 }
 
@@ -29,7 +25,7 @@ const back = document.getElementsByClassName("invisible-cities-each-exit")
 for(var i=0; i< back.length; i++){
     back[i].addEventListener("click", event=>{
         for(var j=0; j<eachPost.length; j++){
-            eachPost[j].classList.add("hidden")    
+            eachPost[j].classList.add("hidden")
         }
     })
 }
